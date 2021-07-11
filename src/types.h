327747a0aa7159bfcfb12e9840b071a72e58e4ee
Ryan Sullivan <ryansully96@googlemail.com>
2021-07-11 20:52:59 +0100

    Initial commit. Turn on LED.

---
+++ src/types.h
@@ -0,0 +1,20 @@
+/**
+ * -------------------------------------------------------------------------------------------------
+ * @author  Ryan Sullivan (ryansullivan@googlemail.com)
+ *
+ * @file    types.h
+ * @brief   Includes modules which define types. Should be included in every source file.
+ *
+ * @date    2021-07-07
+ * -------------------------------------------------------------------------------------------------
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "sfr_defs.h"
+#include "iotn3217.h"
+#include "portpins.h"
+#include "common.h"
+#include "fuse.h"
+#include "lock.h"
